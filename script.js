d428f8cd01318c5e8e0c84844d371f17aed37c67
jsncorn <jsncourn@gmail.com>
2022-01-11 20:10:17 -0800

    removed get languages, added post translate

--- script.js
+++ script.js
@@ -26,6 +26,28 @@ function getQuote() {
       localStorage.setItem(quoteKey, quote)
       console.log(localStorage);
     })
+    
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+        "accept-encoding": "application/gzip",
+        "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+        "x-rapidapi-key": "efc296c17amsh2b92351a9d6aac9p10ae07jsn7e37b7e7e385"
+      },
+      "data": {
+        "q": quote,
+        "target": language,
+        "source": "en"
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
   })
 }
 
@@ -42,18 +64,4 @@ $('#clear-btn').on('click', function () {
   localStorage.clear();
 })
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=option",
-	"method": "GET",
-	"headers": {
-		"accept-encoding": "application/gzip",
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
-		"x-rapidapi-key": "efc296c17amsh2b92351a9d6aac9p10ae07jsn7e37b7e7e385"
-	}
-};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
