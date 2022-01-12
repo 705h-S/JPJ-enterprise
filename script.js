44d88450c2baf27487ddd532fe15709070fd395e
jsncorn <jsncourn@gmail.com>
2022-01-11 19:38:42 -0800

    fixed localstorage

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 function getQuote() {
   $.ajax({
-      url: 'https://api.kanye.rest/',
-      method: "GET"
-    }).then(function(data) {
-      console.log(data.quote);
-      dataQuote = (data.quote);
-      console.log(dataQuote)
-      $("#qoute").text(data.quote).val();
+    url: 'https://api.kanye.rest/',
+    method: "GET"
+  }).then(function (data) {
+    console.log(data.quote);
+    $("#qoute").text(data.quote).val();
 
-
-
-
-
+    $('#refresh-button').on('click', function () {
+      getQuote();
+      refreshCount++;
+      console.log(refreshCount)
     })
+
+    var refreshCount = 0;
+    var saveBtn = $('#save-button');
+    saveBtn.on('click', function (event) {
+      event.preventDefault();
+      var quote = data.quote;
+      var quoteKey = refreshCount;
+      localStorage.setItem(quoteKey, quote)
+      console.log(localStorage(0))
+    })
+
+
+  })
 }
 
 getQuote();
 
-$('#refresh-button').on('click', function() {
-  getQuote();
-  refreshCount++;
-  console.log(refreshCount)
-})
 
-var refreshCount = 0;
-var saveBtn = $('#save-button');
-saveBtn.on('click', function(event) {
-  event.preventDefault();
-  var quote = $('#qoute').val();
-  var quoteKey = refreshCount;
-  localStorage.setItem(quoteKey, quote)
-  console.log(localStorage.getItem(1))
-})
+
