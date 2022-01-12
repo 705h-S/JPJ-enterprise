1d73bb46e91ff77e8cf0518de666471332ddc659
jsncorn <jsncourn@gmail.com>
2022-01-11 19:20:47 -0800

    added localstorage functionality for saving

--- script.js
+++ script.js
@@ -15,5 +15,11 @@ refreshBtn.on('click', function() {
   getQuote();
 })
 
+var refreshCount = 0;
 var saveBtn = $('#save-button');
-console.log(saveBtn)
+saveBtn.on('click', function(event) {
+  event.preventDefault();
+  var quote = $('#qoute');
+  var quoteKey = refreshCount;
+  localStorage.setItem(quoteKey, quote)
+})
